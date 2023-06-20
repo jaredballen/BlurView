369b8c8ebfac475b7ef73471e6e0e2043a02558e
jaredballen <jaredballen@gmail.com>
2023-06-20 16:36:19 -0400

    starting cleanup

--- BlurView.Droid/DirectPort/BlurView.cs
+++ BlurView.Droid/DirectPort/BlurView.cs
@@ -11,7 +11,6 @@ using AndroidX.Annotations;
 using EightBitLab.Com.BlurView;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
-using Xamarin.Forms.Platform.Android.FastRenderers;
 using Color = Android.Graphics.Color;
 using Resource = BlurView.Droid.Resource;
 
@@ -20,6 +19,10 @@ namespace EightBitLab.Com.BlurViewLibrary
 {
     public class BlurView : ViewRenderer
     {
+        private DateTime t0 = DateTime.Now;
+        private List<double> samples = new List<double>();
+        
+        
         private static readonly string TAG = typeof(BlurView).Name;
 
         private IBlurController blurController = new NoOpController();
@@ -27,52 +30,26 @@ namespace EightBitLab.Com.BlurViewLibrary
         private int overlayColor;
         
         private float BlurRadius => (float)((Element as global::BlurView.BlurView)?.BlurRadius ?? global::BlurView.BlurView.DefaultBlurRadius);
-
-        private double RedrawsPerSecond
-        {
-            get => (Element as global::BlurView.BlurView)?.RedrawsPerSecond ?? 0;
-            set
-            {
-                if (Element is not global::BlurView.BlurView blurView) return;
-                blurView.RedrawsPerSecond = value;
-            }
-        }
         
-        public BlurView(Context context) : base(context)
-        {
-            Init(null, 0);
-        }
-        
-        private void Init(IAttributeSet attrs, int defStyleAttr)
-        {
-            TypedArray a = Context.ObtainStyledAttributes(attrs, Resource.Styleable.BlurView, defStyleAttr, 0);
-            overlayColor = a.GetColor(Resource.Styleable.BlurView_blurOverlayColor, Color.Transparent);
-            a.Recycle();
-        }
-
-        private DateTime t0 = DateTime.Now;
-        private List<double> samples = new List<double>();
-        
-        public override void Draw(Canvas canvas)
-        {
-            bool shouldDraw = blurController.Draw(canvas);
-            if (shouldDraw)
-            {
-                base.Draw(canvas);
-            }
-        }
-
         protected override void OnDraw(Canvas? canvas)
         {
             var t1 = DateTime.Now;
             samples.Add((t1 - t0).TotalSeconds);
             t0 = t1;
-            Log.Debug("BlurView", "{0}: rate = {1:F0} Hz", ContentDescription, samples.Count / samples.Sum());
+            Log.Debug("BlurView", "{0}: rate = {1:F0} Hz, location = {2}", ContentDescription, samples.Count / samples.Sum(), $"({this.Left}, {this.Top}, {this.Right}, {this.Bottom})");
             if (samples.Count > 5) samples.RemoveAt(0);
             
             base.OnDraw(canvas);
         }
-
+        
+        public BlurView(Context context) : base(context) { }
+        
+        public override void Draw(Canvas canvas)
+        {
+            if (!blurController.Draw(canvas)) return;
+            base.Draw(canvas);
+        }
+        
         protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
         {
             base.OnSizeChanged(w, h, oldw, oldh);
@@ -84,7 +61,7 @@ namespace EightBitLab.Com.BlurViewLibrary
             base.OnDetachedFromWindow();
             blurController.SetBlurAutoUpdate(false);
         }
-        
+
         protected override void OnAttachedToWindow()
         {
             base.OnAttachedToWindow();
@@ -96,7 +73,7 @@ namespace EightBitLab.Com.BlurViewLibrary
 
             this.SetupWith(currentPage?.GetRenderer()?.View as ViewGroup)
             .SetBlurRadius(BlurRadius);
-                
+            
             if (!IsHardwareAccelerated)
             {
                 Log.Error(TAG, "BlurView can't be used in not hardware-accelerated window!");
@@ -107,18 +84,14 @@ namespace EightBitLab.Com.BlurViewLibrary
             }
         }
 
-        public IBlurViewFacade SetupWith([NonNull] ViewGroup rootView, IBlurAlgorithm algorithm)
+        private IBlurViewFacade SetupWith([NonNull] ViewGroup rootView)
         {
-            this.blurController.Destroy();
-            this.blurController = new PreDrawBlurController(this, rootView, overlayColor, algorithm);
+            blurController.Destroy();
+            blurController = new PreDrawBlurController(this, rootView, overlayColor, Build.VERSION.SdkInt >= BuildVersionCodes.S
+                ? new RenderEffectBlur()
+                : new RenderScriptBlur(Context));
 
             return blurController;
-        }
-
-        [RequiresApi(Value = (int)BuildVersionCodes.JellyBeanMr1)]
-        public IBlurViewFacade SetupWith([NonNull] ViewGroup rootView)
-        {
-            return SetupWith(rootView, GetBlurAlgorithm());
         }
 
         public IBlurViewFacade SetBlurRadius(float radius)
@@ -140,23 +113,6 @@ namespace EightBitLab.Com.BlurViewLibrary
         public IBlurViewFacade SetBlurEnabled(bool enabled)
         {
             return blurController.SetBlurEnabled(enabled);
-        }
-
-        [NonNull]
-        [RequiresApi(Value = (int)BuildVersionCodes.JellyBeanMr1)]
-        private IBlurAlgorithm GetBlurAlgorithm()
-        {
-            IBlurAlgorithm algorithm;
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.S)
-            {
-                algorithm = new RenderEffectBlur();
-            }
-            else
-            {
-                algorithm = new RenderScriptBlur(Context);
-            }
-
-            return algorithm;
         }
     }
 }
